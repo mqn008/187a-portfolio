4c4cb33331239f1742b47cde3e04e3a2ce2aa5b6
Michelle Nguyen <mqn008@ucsd.edu>
2016-03-13 20:56:34 +0000

    about, contacts done. projects in progress

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 
 $(document).ready(function(){
 
-$('a[href^="#"]').on('click',function (e) {
+/*$('a[href^="#"]').on('click',function (e) {
 		e.preventDefault();
 
-		var target = this.hash;
-		var target = $(target);
+		var target = $(this.hash);
 
 		$('ul.tabs li a').removeClass('current-tab');
 		$(this).addClass('current-tab');
 
-		$('html, #whole').stop().animate({
-			scrollTop: target.offset().top - 140 
-		}, 900);
+		$('#whole').stop().animate({
+				'scrollTop': target.offset().top-140
+			}, 900);
+	})*/
+
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current-tab');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current-tab');
+		$("#"+tab_id).addClass('current');
 	})
 });
